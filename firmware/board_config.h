d46ec071a7557329f81ec29269aac61c646fe3c3
Andy-J-B <080.abae@gmail.com>
2025-12-28 21:11:25 -0800

    Remove unused files

--- firmware/board_config.h
+++
@@ -1,7 +0,0 @@
-#ifndef BOARD_CONFIG_H
-#define BOARD_CONFIG_H
-
-#define CAMERA_MODEL_ESP32S3_EYE
-#include "camera_pins.h"
-
-#endif
--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -5,13 +5,32 @@
 // ===========================
 // Select camera model in board_config.h
 // ===========================
-#include "board_config.h"
+
 
 // WiFi credentials
 const char *ssid = "Kevin";
 const char *password = "Cookie1207";
 
 // Constants
+#define PWDN_GPIO_NUM  -1
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM  15
+#define SIOD_GPIO_NUM  4
+#define SIOC_GPIO_NUM  5
+
+#define Y2_GPIO_NUM 11
+#define Y3_GPIO_NUM 9
+#define Y4_GPIO_NUM 8
+#define Y5_GPIO_NUM 10
+#define Y6_GPIO_NUM 12
+#define Y7_GPIO_NUM 18
+#define Y8_GPIO_NUM 17
+#define Y9_GPIO_NUM 16
+
+#define VSYNC_GPIO_NUM 6
+#define HREF_GPIO_NUM  7
+#define PCLK_GPIO_NUM  13
+
 const char *POST_URL = "http://10.0.0.135:8080/image";
 // Set interval for photo taking in ms
 const int photoInterval = 10000; // 10 seconds
