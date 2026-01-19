99efa75070492a054e08f229db3b22c29d2c346a
Andy-J-B <080.abae@gmail.com>
2026-01-18 23:09:48 -0800

    Create web server : make initWebServer funciton

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -4,24 +4,25 @@
 #include "esp_camera.h"
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <WebServer.h>
 #include "secrets.h"
 
-/* ----------  Pin mapping (keep only what your board uses) ---------- */
+/* ----------  Pin mapping ---------- */
 #define PWDN_GPIO_NUM   -1
 #define RESET_GPIO_NUM  -1
 #define XCLK_GPIO_NUM   15
-#define SIOD_GPIO_NUM   4
-#define SIOC_GPIO_NUM   5
+#define SIOD_GPIO_NUM    4
+#define SIOC_GPIO_NUM    5
 #define Y2_GPIO_NUM    11
-#define Y3_GPIO_NUM    9
-#define Y4_GPIO_NUM    8
+#define Y3_GPIO_NUM     9
+#define Y4_GPIO_NUM     8
 #define Y5_GPIO_NUM    10
 #define Y6_GPIO_NUM    12
 #define Y7_GPIO_NUM    18
 #define Y8_GPIO_NUM    17
 #define Y9_GPIO_NUM    16
-#define VSYNC_GPIO_NUM 6
-#define HREF_GPIO_NUM  7
+#define VSYNC_GPIO_NUM  6
+#define HREF_GPIO_NUM   7
 #define PCLK_GPIO_NUM  13
 
 const int photoInterval = 10000; // 10 seconds
@@ -30,10 +31,14 @@ unsigned long lastPhotoTime = 0;
 static void initCamera();
 static void connectWifi();
 static void sendPhoto();
+static void initWebServer();
 
 void setup() {
+  Serial.begin(115200);
+  delay(500);
   initCamera();
   connectWifi();
+  initWebServer();
 }
 
 void loop() {
@@ -46,23 +51,23 @@ void loop() {
 static void initCamera() {
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
+  config.ledc_timer   = LEDC_TIMER_0;
+  config.pin_d0       = Y2_GPIO_NUM;
+  config.pin_d1       = Y3_GPIO_NUM;
+  config.pin_d2       = Y4_GPIO_NUM;
+  config.pin_d3       = Y5_GPIO_NUM;
+  config.pin_d4       = Y6_GPIO_NUM;
+  config.pin_d5       = Y7_GPIO_NUM;
+  config.pin_d6       = Y8_GPIO_NUM;
+  config.pin_d7       = Y9_GPIO_NUM;
+  config.pin_xclk     = XCLK_GPIO_NUM;
+  config.pin_pclk     = PCLK_GPIO_NUM;
+  config.pin_vsync    = VSYNC_GPIO_NUM;
+  config.pin_href     = HREF_GPIO_NUM;
   config.pin_sccb_sda = SIOD_GPIO_NUM;
   config.pin_sccb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
+  config.pin_pwdn     = PWDN_GPIO_NUM;
+  config.pin_reset    = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
   config.frame_size = FRAMESIZE_UXGA;
   config.pixel_format = PIXFORMAT_JPEG;
@@ -144,6 +149,28 @@ static void connectWifi()
 }
 
 
+static void initWebServer() {
+  server.on("/", HTTP_GET, []() {
+    server.send(200, "text/plain", "ESP32-CAM Weapon‑Detection - GET /save to store a picture onto sd card");
+
+  });
+  server.on("/save", HTTP_GET, []() {
+    BaseType_t higher = pdFALSE;
+    if (xSemaphoreGiveFromISR(captureSem, &higher) == pdTRUE) {
+      if(higher==pdTRUE) portYIELD_FROM_ISR();
+      server.send(200, "application/json:, "{\"status\":\"capture_queued\"}");
+
+    } else {
+      server.send(500, "application/json", "{\"error\":\"queue_full_or_semaphore_error\"}")
+    }
+  });
+  server.onNotFound([]() {
+    server.send(404, "text/plain", "Not Found");
+  });
+
+  server.begin();
+  Serial.println("HTTP server started – listen on port 80");
+}
 
 static void sendPhoto () {
   if (WiFi.status() != WL_CONNECTED) {
